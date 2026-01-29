8e4d63c0b6ab34ddc1dfbb93aa72abb0bd797b4b
Anish <anishvayoli@gmail.com>
2026-01-29 23:05:03 +0530

    fix: Set DEBUG to False for production readiness

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mg48q1!ru=wfkkf!&@kx4dgo=&c+9oea3y_xj%*$yzsk$-q%#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gymnospermic-stepanie-integumentary.ngrok-free.dev', '127.0.0.1', 'localhost']
 
