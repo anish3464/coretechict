dcc48436600a900f4695e496c825789c09c23aa8
Anish <anishvayoli@gmail.com>
2026-01-29 21:13:26 +0530

    Revert "Changes to testimonials, parteners and projects"
    This reverts commit 681bfe0bd75e4680d42eec285bf58c186d1fb534.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from website.views import home, services, contact, cyber_security, network_security, collaboration, digital_transformation, chat_submit
+from website.views import home, services, partners, projects, contact, cyber_security, network_security, collaboration, digital_transformation
 from django.contrib import admin
 from django.urls import path
 
@@ -22,10 +22,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('services/', services, name='services'),
+    path('partners/', partners, name='partners'),
+    path('projects/', projects, name='projects'),
     path('contact/', contact, name='contact'),
     path('services/cyber-security/', cyber_security),
     path('services/network-security/', network_security),
     path('services/collaboration/', collaboration),
     path('services/digital-transformation/', digital_transformation),
-    path('api/chat-submit/', chat_submit, name='chat_submit'),
 ]
